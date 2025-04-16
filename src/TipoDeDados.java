fe9f055aa43a0f55a4a34518b72c5c2b1d75c62e
AquillaSG <aquillagomes@hotmail.com>
2025-04-16 19:24:50 -0300

    updated tipos de dados class

--- src/TipoDeDados.java
+++ src/TipoDeDados.java
@@ -1,5 +1,5 @@
 public class TipoDeDados {
-    public static void main(String[] args) throws exception {
+    public static void main(String[] args) {
         byte idade = 123;
         short ano = 2025;
         int cep = 12345678;
@@ -9,5 +9,15 @@ public class TipoDeDados {
         char letra = 'A';
         boolean isTrue = true; // true ou false
         String nome = "João";
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano); 
+        System.out.println("CEP: " + cep);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Pi: " + pi);
+        System.out.println("Salário: " + salario);
+        System.out.println("Letra: " + letra);
+        System.out.println("É verdade: " + isTrue);
+        System.out.println("Nome: " + nome);
     }
 }
