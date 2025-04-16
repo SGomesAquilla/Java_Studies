0c1350643d1ad6a571d40ac2413605e3f9252172
AquillaSG <aquillagomes@hotmail.com>
2025-04-16 19:17:12 -0300

    tipos de variaveis

---
+++ src/TipoDeDados.java
@@ -0,0 +1,13 @@
+public class TipoDeDados {
+    public static void main(String[] args) throws exception {
+        byte idade = 123;
+        short ano = 2025;
+        int cep = 12345678;
+        long cpf = 12345678901L;
+        float pi = 3.14f;
+        double salario = 1234.56;
+        char letra = 'A';
+        boolean isTrue = true; // true ou false
+        String nome = "João";
+    }
+}
